0f243ec4db63a1600e337296e3ce440e536a3751
DaDDy <blacklistuser420@gmail.com>
2022-12-28 18:38:49 +0630

    react exe complete

---
+++ ztm/clothing/src/App.js
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.components";
+import Home from "./routes/home/home.component";
+import SignIn from "./routes/sign-in.component/sign-in.component";
+import "./App.css";
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
